41db97eed4e3f10ccd7a9c6e9d847498725b1738
Jameel Socorro <jameelsocorro@gmail.com>
2018-10-05 21:40:32 +0800

    moved files into source_code folder

--- index.js
+++
@@ -1,6 +0,0 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-
-
-
-ReactDOM.render(<div>test</div>, document.getElementById('app'));
---
+++ source_code/index.js
@@ -0,0 +1,6 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './src/app';
+
+ReactDOM.render(<App />, document.getElementById('app'));
